905a7ef3920eb421fac417875937663bb9b7b5ca
Lyle <494205736@qq.com>
2020-06-13 13:57:18 +0800

    项目新建,初始化

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import store from "./store";
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+const router = new VueRouter({
+  routes: [],
+});
+/* eslint-disable no-new */
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  devServer: {
+    host: "localhost",
+    port: 8080,
+    proxy: {
+      "/activity": {
+        target: "https://www.imooc.com",
+        changeOrigin: false,
+      },
+    },
+  },
+};
+
+};
